0d7e0767488530ab20284153aad70f77ba722695
Lucas Vianna <lucas.araujo580@gmail.com>
2020-09-20 19:56:04 -0300

    Projeto Recriado após erro na pasta .git

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/header';
+import Records from './pages/Records';
+import Charts from './pages/Charts';
+const Routes = () => (
+<BrowserRouter>
+<Header  />
+<Switch>
+    <Route path="/" exact>
+        <Home />
+    </Route>
+    <Route path="/records" >
+        <Records />
+    </Route>
+    <Route path="/charts" >
+        <Charts />
+    </Route>
+</Switch>
+</BrowserRouter>
+);
+
+export default Routes;
